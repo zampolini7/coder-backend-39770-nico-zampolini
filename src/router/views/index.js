3af32ac3522759c070dfd0e68576ea63b033b786
git <zampolini7@gmail.com>
2023-05-16 09:48:40 -0300

    Avances en entregable 5

--- src/router/views/index.js
+++ src/router/views/index.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import products_router from "./products.js";
+import product_router from "./product.js";
+import cart_router from "./carts.js";
+
 const views_router = Router();
 
 views_router.use("/", products_router);
+views_router.use("/", product_router);
+views_router.use("/", cart_router);
 
 export default views_router;
---
+++ src/router/views/product.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+const product_router = Router();
+
+product_router.get("/products/:pid", async (req, res, next) => {
+  let { pid } = req.params;
+
+  const response = await fetch(`http://localhost:8080/api/products/${pid}`);
+  const responseJson = await response.json();
+  const product = responseJson.response;
+  try {
+    return res.render("product", {
+      title: "product",
+      // script: "product.js",
+      product: product,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default product_router;
