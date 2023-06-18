25815a13941f04cc520d204bed57bc89c909ba4a
git <zampolini7@gmail.com>
2023-06-18 18:52:31 -0300

    Avances en eliminar handlebars

---
+++ src/router/api/carts.js
@@ -0,0 +1,169 @@
+import { Router } from "express";
+// import cart from "../../dao/managers/cartScript.js";
+import Cart from "../../models/Cart.js";
+const router = Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    let limit = req.query.limit ?? null;
+    let carts = await Cart.find();
+    if (limit !== null) {
+      res.json({
+        status: "200",
+        response: carts.slice(0, limit),
+      });
+    }
+    if (limit === null && carts.length > 0) {
+      res.json({
+        status: "200",
+        response: carts,
+      });
+    } else {
+      res.json({
+        status: "200",
+        response: "No hay productos en el carrito",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:cid", async (req, res, next) => {
+  try {
+    let { cid } = req.params;
+    console.log(cid);
+    // let cart2 = await cart.getCartById(cid);
+    let cart2 = await Cart.findById(cid);
+
+    if (cart2 !== null) {
+      res.json({
+        status: "200",
+        response: cart2,
+      });
+    } else {
+      res.json({
+        status: "404",
+        response: "Producto no encontrado",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newCart = req.body;
+    const { products } = newCart;
+    console.log(products);
+
+    if (products) {
+      // let cartCreated = await cart.addCart(products ?? []);
+      let cartCreated = await Cart.Create(products ?? []);
+
+      res.json({
+        status: "200",
+        response: cartCreated,
+      });
+    } else {
+      res.json({
+        status: "404",
+        response: "Faltan campos por completar",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+router.put("/:cid/product/:pid/:units", async (req, res, next) => {
+  try {
+    let { cid, pid, units } = req.params;
+    console.log(req.params, "req.para");
+    if (cid && pid && units) {
+      // let productUpdated = await cart.updateProduct(cid, pid, units);
+      let productUpdated = await Cart.findByIdAndUpdate(cid, pid, units);
+
+      if (
+        productUpdated !== null &&
+        !productUpdated.includes(
+          "Las cantidades que quieres ingresar superan el stock"
+        )
+      ) {
+        res.json({
+          status: "200",
+          response: productUpdated,
+        });
+      } else if (
+        productUpdated !== null &&
+        productUpdated.includes(
+          "Las cantidades que quieres ingresar superan el stock"
+        )
+      ) {
+        res.json({
+          status: "404",
+          response: productUpdated,
+        });
+      } else {
+        res.json({
+          status: "404",
+          response: productUpdated,
+        });
+      }
+    } else {
+      res.json({
+        status: "400",
+        response: "Parámetros incompletos",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:cid/product/:pid/:units", async (req, res, next) => {
+  try {
+    let { cid, pid, units } = req.params;
+
+    if (cid && pid && units) {
+      // let productDeleted = await cart.deleteProduct(cid, pid, units);
+      let productDeleted = await Cart.findByIdAndDelete(cid);
+
+      if (
+        productDeleted !== null &&
+        !productDeleted.includes(
+          "La cantidad que quieres quitar supera la cantidad en el carrito"
+        )
+      ) {
+        res.json({
+          status: "200",
+          response: productDeleted,
+        });
+      } else if (
+        productDeleted !== null &&
+        productDeleted.includes(
+          "La cantidad que quieres quitar supera la cantidad en el carrito"
+        )
+      ) {
+        res.json({
+          status: "404",
+          response: productDeleted,
+        });
+      } else {
+        res.json({
+          status: "404",
+          response: productDeleted,
+        });
+      }
+    } else {
+      res.json({
+        status: "400",
+        response: "Parámetros incompletos",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import auth_router from "./auth.js";
+import products_router from "./api/products.js";
+import cart_router from "./api/carts.js";
 
 const index_router = Router();
 
 index_router.use("/auth", auth_router);
+index_router.use("/products", products_router);
+index_router.use("/carts", cart_router);
 
 export default index_router;
